1b5280a67ba20d7d4469f6048508c58b9a8cce5c
ebrargur <ebrarusa@gmail.com>
2020-10-24 10:59:28 -0400

    Array practices

--- src/October21th/HW3Countries.java
+++ src/October21th/HW3Countries.java
@@ -1,31 +1,37 @@
 package October21th;
 
-
 public class HW3Countries {
 
 	public static void main(String[] args) {
-		String[] countries = {"Turkey", "USA", "Canada"};
-		
-		
-		
-		countries[0]="Ankara";
-		countries[1]="Washington";
-		countries[2]="Toronto";
-				
-		for(int i=0;i<countries.length;i++)
-			{
-				
-			//	System.out.println(countries[i]+" ");
-				System.out.println("The capital of index "+ (i)+":"+countries[i]+" ");
-			
-			}
-		// System.out.println(Arrays.todeepString(countries));
-		 
-		for(String capital:countries)
-		{
-			System.out.print(capital +" ");
+		String[] countries = { "Turkey", "USA", "Canada" };
+        String[] capitals= {"Ankara","Washington","Ottawa"};
 
-		
+	/*	for (int i = 0; i < countries.length; i++) {
+
+			System.out.println("The capital of index " + (i) + ":" + countries[i] + " ");
+
+		}*/
+		String capital;
+		for (String country : countries) {
+			switch (country) {
+			case "Turkey":
+
+				capital = "Ankara";
+				break;
+			case "USA":
+				capital = "Washington";
+				break;
+			case "Canada":
+				capital = "Ottawa";
+				break;
+			default:
+
+				capital = "I dont know";
+
+			}
+			
+			System.out.println("The capital of " + country + ":" +capital);
+		}
+
+	}
 }
-		
-	}}
