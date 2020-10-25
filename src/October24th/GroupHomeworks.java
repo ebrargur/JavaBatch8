fed6dbe799ffe700878c7c435bdc07877e9d8b76
ebrargur <ebrarusa@gmail.com>
2020-10-25 10:41:42 -0400

    Group Assignments

---
+++ src/October24th/GroupHomeworks.java
@@ -0,0 +1,32 @@
+package October24th;
+
+public class GroupHomeworks {
+/*
+ * Write a program to swap 2 numbers without a temporary variable? Swap  2 strings without a temporary variable?
+
+
+Write a java program to check whether a given number is prime or not?
+
+
+Write a Java Program to print the first 10 numbers of Fibonacci series.
+
+
+Maximum and minimum number in the array?
+
+
+Write a java program to find the second largest number in the array?
+
+
+Create a 2D array to store numbers in 3 rows and 3 columns. Print the sum of all numbers.
+
+
+Create a 2D array to store numbers in 3 rows and 4 columns. Develop a program which will identify/print the even numbers only.
+
+
+Create a 2D array to store numbers and calculate sum of all odd numbers.
+
+
+Create an array of countries: north america countries, south america countries, europe countries, asian countries, african countries. Then print all values from that array using 2 different loops and calculate how many total countries been stored.
+ * 
+ * */
+}
